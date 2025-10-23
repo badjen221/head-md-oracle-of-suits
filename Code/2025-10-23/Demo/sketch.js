b6493373fd2f4f26fc591c7afd584ab6ad51c4da
Rokhy NIANG <badjenian@gmail.com>
2025-10-23 11:29:52 +0200

    Circle resize if we blink eyes

--- Code/2025-10-23/Demo/sketch.js
+++ Code/2025-10-23/Demo/sketch.js
@@ -40,32 +40,37 @@ function draw() {
   textAlign(LEFT, TOP);
   text('Left Eye Blink: ' + nf(leftEyeBlink, 1, 2), 10, height - 100);
 
-  // if we blink rightEye, draw a circle
-  rightEyeBlink = getBlendshapeScore('eyeBlinkRight');
-  if (rightEyeBlink > 0.5) {
-    let numBubbles = map(rightEyeBlink, 1.5, 1.0, 10, 20);
-    for (let i = 0; i < numBubbles; i++) {
-      let x = random(width);
-      let y = random(height);
-      let size = random(10, 60);
-      noStroke();
-      fill(200, 90, 0, 150);
-      ellipse(x, y, size, size);
-    }
+  // if we open jaw, draw a rectangle
+  jawOpen = getBlendshapeScore('jawOpen');
+  if (jawOpen > 0.4) {
+    let rectWidth = map(jawOpen, 0.5, 1.0, 50, 200);
+    let rectHeight = map(jawOpen, 0.5, 1.0, 20, 100);
+    stroke(1);
+    fill(0, 200, 100, 140);
+    rectMode(CENTER);
+    rect(width / 2, 1 * height / 3, rectWidth, rectHeight);
   }
 
-  // if we open jaw, draw many bubbles
-  jawOpen = getBlendshapeScore('jawOpen');
-  if (jawOpen > 0.5) {
-    let numBubbles = map(jawOpen, 1.5, 1.0, 10, 80);
-    for (let i = 0; i < numBubbles; i++) {
-      let x = random(width);
-      let y = random(height);
-      let size = random(10, 60);
-      noStroke();
-      fill(255, 255, 0, 150);
-      ellipse(x, y, size, size);
-    }
+  // if we blink rightEye, draw a circle
+  rightEyeBlink = getBlendshapeScore('eyeBlinkRight');
+   if (rightEyeBlink < 0.3) {
+    let rectWidth = map(rightEyeBlink, 0.5, 1.0, 50, 200);
+    let rectHeight = map(rightEyeBlink, 0.5, 1.0, 20, 100);
+    stroke(1);
+    fill(0, 170, 200, 140);
+    rectMode(CENTER);
+    circle(1.3* width / 3, height / 4, rectWidth, rectHeight);
+  }
+
+  // if we blink leftEye, draw a circle
+  leftEyeBlink = getBlendshapeScore('eyeBlinkLeft');
+   if (leftEyeBlink < 0.3) {
+    let rectWidth = map(leftEyeBlink, 0.5, 1.0, 50, 200);
+    let rectHeight = map(leftEyeBlink, 0.5, 1.0, 20, 100);
+    stroke(1);
+    fill(200, 0, 150, 140);
+    rectMode(CENTER);
+    circle(1.8 * width / 3, height / 4, rectWidth, rectHeight);
   }
 }
 
